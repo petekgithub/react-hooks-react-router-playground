a4ff08d567aa073745d34f1bb7f197ac1b71e1f5
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-07-16 21:56:15 +0300

    component-update

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <h1>About page</h1>
+        <h1>About component</h1>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <h1>Navbar</h1>
+        <h1>Navbar component</h1>
     </div>
   )
 }
