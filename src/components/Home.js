3415bbe8bd9bf994e5e3a7d6d21bd8d96669e032
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-07-17 01:16:18 +0300

    router-update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({isLoggedIn}) => {
+
+  if(!isLoggedIn) return <Navigate to="/login"/>;
+
   return (
     <div>
         <h1>Home component</h1>
