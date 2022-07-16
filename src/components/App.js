ccbbde164295eb7418ad9d64034ad619533e7508
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-07-16 21:52:44 +0300

    react-hooks-router1

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+
+
+
+
+const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className='App'>
+        <Navbar setIsLoggedIn={setIsLoggedIn}/>
+        <Router>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+                <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+            </Routes>
+        </Router>
+    </div>
+  )
+}
+
+export default App
